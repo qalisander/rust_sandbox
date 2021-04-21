aad1656d28b3bad153aea9b7e25c0bc106f22df5
qalisander <qalisander@gmail.com>
2021-04-21 16:48:15 +0300

    spiral2

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod sudoku;
 pub mod sudoku1;
 pub mod sudoku2;
 pub mod spiral;
+pub mod spiral2;
--- src/spiral.rs
+++ src/spiral.rs
@@ -3,33 +3,21 @@ pub fn spiralize(size: usize) -> Vec<Vec<i8>> {
         .map(|_| vec![0 as i8; size])
         .collect::<Vec<Vec<_>>>();
 
-    // missing lifetime spicifier
-    // https://depth-first.com/articles/2020/06/22/returning-rust-iterators/
-
-    // TODO: recursive iterators in Rust https://fasterthanli.me/articles/recursive-iterators-rust
-
-    // TODO: use size - 2, not x = -2
     for (x, y) in spiralize_rec((-2, 0), (1, 0), size as isize + 1) {
         spiral[y as usize][x as usize] = 1;
     }
 
     return spiral;
 
-    fn spiralize_rec(
-        ind: (isize, isize),
-        d_ind: (isize, isize),
-        l: isize,
-    ) -> impl Iterator<Item = (isize, isize)> {
-        // TODO: why we can't use it without box
-        // compare boxed iterators with vectors in benchmark
+    fn spiralize_rec(ind: (isize, isize), d_ind: (isize, isize), l: isize) -> Vec<(isize, isize)> {
         return if l <= 0 {
-            vec![].into_iter()
+            vec![]
         } else if l == 1 {
-            vec![(ind.0 + d_ind.0, ind.1 + d_ind.1)].into_iter()
+            vec![(ind.0 + d_ind.0, ind.1 + d_ind.1)]
         } else {
             (1..=l)
                 .map(|i| {
-                    (ind.0 + i * d_ind.0, ind.1 + i * d_ind.1) // BUG
+                    (ind.0 + i * d_ind.0, ind.1 + i * d_ind.1)
                 })
                 .filter(|p| p.0 >= 0 && p.1 >= 0)
                 .chain(spiralize_rec(
@@ -38,10 +26,17 @@ pub fn spiralize(size: usize) -> Vec<Vec<i8>> {
                     if d_ind.1 == 0 { l - 2 } else { l },
                 ))
                 .collect::<Vec<(isize, isize)>>()
-                .into_iter()
         };
-        // return Box::new(        if l > 0 {
-        //     iter.chain(spiralize_rec(x, y, -dy, dx, if dy == 0 { l - 2 } else { l })).collect()
-        // } else {iter.into_iter()});
     }
 }
+
+// missing lifetime spicifier
+// https://depth-first.com/articles/2020/06/22/returning-rust-iterators/
+// TODO: recursive iterators in Rust https://fasterthanli.me/articles/recursive-iterators-rust
+
+// TODO: why we can't use it without box
+// compare boxed iterators with vectors in benchmark
+
+// return Box::new(        if l > 0 {
+//     iter.chain(spiralize_rec(x, y, -dy, dx, if dy == 0 { l - 2 } else { l })).collect()
+// } else {iter.into_iter()});
