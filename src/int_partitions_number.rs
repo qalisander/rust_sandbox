9647daf486ef9193a1a3e8ece569545f75180696
Alisander Qoshqosh <qalisander@gmail.com>
2021-08-09 05:00:57 +0300

    int_partitions_number: solution with memo

--- src/int_partitions_number.rs
+++ src/int_partitions_number.rs
@@ -1,29 +1,33 @@
 // https://www.codewars.com/kata/546d5028ddbcbd4b8d001254/train/rust
 
-use std::collections::HashMap;
-use itertools::Itertools;
-// #![feature(test)]
+use std::collections::{HashMap};
 
+// TODO: compare with criterion: BTreeMap, HashMap and BinaryHeap
 fn partitions(num: isize) -> isize {
-    // let mut memory = HashMap::new();
-    return part_rec(num, num/*, &mut memory*/);
+    let mut memory = HashMap::new();
+    return part_rec(num, num, &mut memory);
     
-    fn part_rec(num: isize, max_num: isize/*, memory: &mut HashMap<isize, isize>*/) -> isize {
-/*        if let Some(&ans) = memory.get(&num) {
+    fn part_rec(num: isize, max_num: isize, memory: &mut HashMap<(isize, isize), isize>) -> isize {
+        if let Some(&ans) = memory.get(&(num, max_num)) {
             return ans;
-        }*/
-        
-        if num == 0 { 1 } else {
-            (1..=num.min(max_num)).map(|delta| {
-                part_rec(num - delta, delta.min(num - delta))
-            }).sum()
         }
+        
+        let ans = if num == 0 { 1 } else {
+            (1..=num.min(max_num)).map(|delta| {
+                part_rec(num - delta, delta.min(num - delta), memory)
+            }).sum()
+        };
+        
+        memory.insert((num, max_num), ans);
+        ans
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    extern crate test;
+    use test::Bencher;
 
     #[test]
     fn basic_test_01() {
@@ -44,9 +48,25 @@ mod tests {
     fn basic_test_25() {
         assert_eq!(partitions(25), 1958);
     }
+    
+    #[test]
+    fn basic_test_70() {
+        assert_eq!(partitions(70), 4087968);
+    }
 
-    // #[bench]
-    // fn basic_test_100() {
-    //     assert_eq!(partitions(50), 1958);
-    // }
+    #[bench]
+    fn bench_test_70(bencher: &mut Bencher) {
+        bencher.iter(||{
+            partitions(70)
+        });
+    }
+
+    #[bench]
+    fn bench_test_100(bencher: &mut Bencher) {
+        bencher.iter(||{
+            partitions(100)
+        });
+    }
 }
+
+    
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(test)]
 pub mod sudoku;
 pub mod sudoku_2;
 pub mod sudoku_3;
