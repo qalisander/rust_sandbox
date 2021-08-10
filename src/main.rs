6cad8af0d5897e153c4ec13a2c1eae404dba0250
Alisander Qoshqosh <qalisander@gmail.com>
2021-08-10 03:25:07 +0300

    rail_fence_cipher: encode

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use itertools::Itertools;
 
 fn main() {
-    let vct = (1..=3/2) .collect_vec();
-    println!("{:?}", vct)
+// Build a vector of the strings "101", "102", ... "105"
+    let mut v = Vec::new();
+    for i in 101 .. 106 {
+        v.push(i.to_string());
+    }
+// 1. Pop a value off the end of the vector:
+    let fifth = v.pop().expect("vector empty!");
+    assert_eq!(fifth, "105");
+// 2. Move a value out of a given index in the vector,
+// and move the last element into its spot:
+    let second = v.swap_remove(1);
+    assert_eq!(second, "102");
+// 3. Swap in another value for the one we're taking out:
+    let third = std::mem::replace(&mut v[2], "substitute".to_string());
+    assert_eq!(third, "103");
+// Let's see what's left of our vector.
+    assert_eq!(v, vec!["101", "104", "substitute"]);
 }
 
 // struct Solution;
--- src/rail_fence_cipher.rs
+++ src/rail_fence_cipher.rs
@@ -1,7 +1,19 @@
 // https://www.codewars.com/kata/58c5577d61aefcf3ff000081/train/rust
 
+use itertools::Itertools;
+
 fn encode_rail_fence_cipher(text: &str, num_rails: usize) -> String {
-    unimplemented!()
+    (0..num_rails).flat_map(|rail_id| {
+         text.chars().enumerate().filter_map(move |(id, ch)| {
+            let modulo = (num_rails - 1) * 2;
+            if (id + rail_id) % modulo * 2 == 0 || (modulo + id - rail_id) % modulo * 2 == 0
+            {
+                Some(ch)
+            } else {
+                None
+            }
+        })
+    }).collect::<String>()
 }
 
 fn decode_rail_fence_cipher(text: &str, num_rails: usize) -> String {
@@ -15,8 +27,8 @@ mod tests {
     #[test]
     fn basic_tests() {
         assert_eq!(encode_rail_fence_cipher("WEAREDISCOVEREDFLEEATONCE", 3), "WECRLTEERDSOEEFEAOCAIVDEN");
-        assert_eq!(decode_rail_fence_cipher("WECRLTEERDSOEEFEAOCAIVDEN", 3), "WEAREDISCOVEREDFLEEATONCE");
+        // assert_eq!(decode_rail_fence_cipher("WECRLTEERDSOEEFEAOCAIVDEN", 3), "WEAREDISCOVEREDFLEEATONCE");
         assert_eq!(encode_rail_fence_cipher("Hello, World!", 3), "Hoo!el,Wrdl l");
-        assert_eq!(decode_rail_fence_cipher("Hoo!el,Wrdl l", 3), "Hello, World!");
+        // assert_eq!(decode_rail_fence_cipher("Hoo!el,Wrdl l", 3), "Hello, World!");
     }
 }
