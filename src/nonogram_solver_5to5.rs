2282fc6bac9af1d20a84b99fc7dc731ebd51535b
Alisander Qoshqosh <qalisander@gmail.com>
2021-09-18 21:20:31 +0300

    nonogram_solver_5to5: ++

--- src/nonogram_solver_5to5.rs
+++ src/nonogram_solver_5to5.rs
@@ -1,61 +1,80 @@
 use itertools::Itertools;
-use std::fmt::Display;
+use std::fmt::{Display, Debug};
+use std::convert::TryInto;
+
+struct Clue {
+    init_size: u8,
+    status: ClueStatus,
+}
+
+enum ClueStatus {
+    NotUsed,
+    InUse {
+        current_size: u8,
+    },
+    Used,
+}
+
+//    |-------> (x)
+//    |
+//    |
+//    V (y)
 
 // https://www.codewars.com/kata/5a479247e6be385a41000064/train/rust
-fn solve_nonogram((top_clues, left_clues): ([&[u8]; 5], [&[u8]; 5])) -> [[u8; 5]; 5] {
-    //    |-------> (x)
-    //    |
-    //    |
-    //    V (y)
+fn solve_nonogram<const T: usize>((top_clues, left_clues): ([&[u8]; T], [&[u8]; T])) -> [[u8; T]; T] {
 
-    fn solve_nongram_rec(
-        y_index: u32,
-        (top_clues, left_clues): &([&[u8]; 5], [&[u8]; 5]),
-        field: &mut [[u8; 5]; 5],
-    ) -> bool {
-        todo!("fn: partition by clue");
-        todo!("validate recursion");
-    }
+    let mut permutations_stack: Vec<[u8; T]> = Vec::with_capacity(T);
 
-    let mut field = [[0u8; 5]; 5];
-    print(&field);
+    if solve_nongram_rec(&(top_clues, left_clues), &mut permutations_stack) {
+        return permutations_stack.try_into().unwrap(); // https://stackoverflow.com/questions/29570607/is-there-a-good-way-to-convert-a-vect-to-an-array
+    } else { 
+        panic!("Solution not found")
+    };
 
     // TODO: prlly create two functions: validate_field and validate_strategy (that stops further recursion)
-    if solve_nongram_rec(0, &(top_clues, left_clues), &mut field) {
-        field
-    } else {
-        panic!("Solution not found")
+    fn solve_nongram_rec<const T: usize>(
+        (top_clues, left_clues): &([&[u8]; T], [&[u8]; T]),
+        permutations_stack: &mut Vec<[u8; T]>,
+    ) -> bool {
+        unimplemented!();
     }
+
+    let mut field = [[0u8; T]; T];
+    print(&field);
+    field
 }
 
 //TODO: use const generics https://blog.rust-lang.org/2021/02/26/const-generics-mvp-beta.html
 //TODO: pass here arr with len: u8
 //TODO: pay attention to boundaries between clues (there is at least one space between them)
 //TODO: write get_permutations_test
+//TODO: try run Rust code from python test
 
+//TODO: distance between permutations
 fn get_permutations<const N: usize>(clues: &'static [u8], init_offset: usize) -> Box<dyn Iterator<Item=Box<[u8; N]>>> {
     if clues.is_empty() {
         return Box::new(std::iter::once(Box::new([0; N])));
     }
 
     let current_clue = *clues.first().unwrap() as usize;
-
-    Box::new((0..=N - init_offset - clues.iter().sum::<u8>() as usize)
+    let clues_sum = clues.iter().sum::<u8>() as usize;
+    
+    Box::new((0..=N.saturating_sub(init_offset + clues_sum))
         .flat_map(move |offset| {
             let offset = init_offset + offset;
-            get_permutations(&clues[1..], offset + current_clue)
+            get_permutations(&clues[1..], 1 + offset + current_clue)
                 .map(move |mut slice| {
                     slice[offset..current_clue + offset].fill(1);
-//                    slice.iter_mut().skip(offset).take(current_clue).for_each(|mut item| *item = 1);
                     slice
                 })
         }))
 }
 
-fn print(field: &[[u8; 5]; 5]) {
+fn print<T: Debug>(field: impl IntoIterator<Item=impl IntoIterator<Item=T>>) {
     println!(
         "{}",
-        field.iter().map(|slice| format!("{:?}", slice)).join("\n")
+        field.into_iter().map(|collection| 
+            format!("{:?}", collection.into_iter().collect_vec())).join("\n")
     );
 }
 
