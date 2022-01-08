1a7ff65e8628e72936e7a54f56efb604ccb48231
Alisander Qoshqosh <qalisander@gmail.com>
2022-01-08 03:53:44 +0300

    transforming_maze_solver: debug fmt

--- src/transforming_maze_solver/transforming_maze_solver_sol.rs
+++ src/transforming_maze_solver/transforming_maze_solver_sol.rs
@@ -2,6 +2,7 @@
 
 use crate::transforming_maze_solver::transforming_maze_solver_sol::Tile::Unvisited;
 use itertools::Itertools;
+use std::fmt::{Debug, Formatter};
 
 type DIR = i8;
 
@@ -16,8 +17,9 @@ const END: DIR = -2;
 #[derive(Debug, Copy, Clone)]
 enum Tile {
     Visited {
-        prev_tile_index: (i8, i8),
+        prev_tile_delta: (i8, i8),
         prev_grid_index: i8,
+        walls: DIR,
     },
     Unvisited {
         walls: DIR,
@@ -28,28 +30,94 @@ enum Tile {
 
 type Grid = Vec<Vec<Tile>>;
 struct Field {
-    grid: [Grid; 4],
+    grids: [Grid; 4],
     begin: (i8, i8),
     end: (i8, i8),
+}
+
+impl Debug for Field {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f)?;
+        for (index, grid) in self.grids.iter().enumerate() {
+            writeln!(f, "Grid: {}", index)?;
+            let i_max = grid.len();
+            let j_max = grid[0].len();
+
+            let grid = grid
+                .iter()
+                .map(|row| {
+                    row.iter()
+                        .map(|tile| match *tile {
+                            Tile::Visited {
+                                walls,
+                                prev_tile_delta,
+                                ..
+                            } => format_walls(walls, get_dir_char(prev_tile_delta)),
+                            Tile::Unvisited { walls } => format_walls(walls, '·'),
+                            Tile::Begin => format_walls(0, 'B'),
+                            Tile::End => format_walls(0, 'X'),
+                        })
+                        .collect_vec()
+                })
+                .collect_vec();
+
+            for str_row_index in 0..i_max * 3 {
+                let row_index = str_row_index / 3;
+                let index = str_row_index % 3;
+                let str = grid[row_index]
+                    .iter()
+                    .flat_map(|arr| arr[index..(3 + index)].iter())
+                    .collect::<String>();
+                writeln!(f, "{}", str)?;
+            }
+        }
+        return writeln!(f);
+
+        fn format_walls(walls: DIR, ch_inside: char) -> [char; 9] {
+            let mut formatted_dir = [' '; 9];
+            formatted_dir[4] = ch_inside;
+            if walls & N_DIR != 0 {
+                formatted_dir[1] = '—'
+            }
+            if walls & W_DIR != 0 {
+                formatted_dir[3] = '|'
+            }
+            if walls & E_DIR != 0 {
+                formatted_dir[5] = '|'
+            }
+            if walls & S_DIR != 0 {
+                formatted_dir[7] = '—'
+            }
+            formatted_dir
+        }
+    }
+}
+
+fn get_dir_char(delta: (i8, i8)) -> char {
+    match delta {
+        (i, _) if i < 0 => 'N',
+        (i, _) if i > 0 => 'S',
+        (_, j) if j < 0 => 'E',
+        (_, j) if j < 0 => 'W',
+        _ => panic!("Invalid delta: {:?}", delta),
+    }
 }
 
 impl Field {
     fn new(maze: &Vec<Vec<DIR>>) -> Self {
         let mut begin = None;
         let mut end = None;
-        let mut process_dir = |dir, (i, j)| {
-            match dir {
-                BEGIN => {
-                    begin = Some((i as i8, j as i8));
-                    Tile::Begin
-                }
-                END => {
-                    end = Some((i as i8, j as i8));
-                    Tile::End
-                }
-                dir if dir & DIR_MASK == dir => Tile::Unvisited { walls: dir },
-                dir => panic!("Invalid cell value: {}", dir),
+        let mut process_dir = |dir, (i, j)| match dir {
+            BEGIN => {
+                begin = Some((i as i8, j as i8));
+                Tile::Begin
             }
+            END => {
+                end = Some((i as i8, j as i8));
+                Tile::End
+            }
+            dir if dir & DIR_MASK == dir => Tile::Unvisited { walls: dir },
+            dir => panic!("Invalid cell! value: {}; index: {:?}", dir, (i, j)),
         };
 
         let grid = (0..4)
@@ -59,7 +127,14 @@ impl Field {
                     .map(|(i, row)| {
                         row.iter()
                             .enumerate()
-                            .map(|(j, dir)| process_dir(*dir, (i, j)))
+                            .map(|(j, &dir)| {
+                                let dir = if dir & DIR_MASK != dir {
+                                    dir
+                                } else {
+                                    shift_dir(dir, shift)
+                                };
+                                process_dir(dir, (i, j))
+                            })
                             .collect_vec()
                     })
                     .collect_vec()
@@ -68,8 +143,8 @@ impl Field {
             .try_into()
             .unwrap();
 
-        Field{
-            grid,
+        Field {
+            grids: grid,
             begin: begin.expect("Begin cell not found!"),
             end: end.expect("End cell not found!"),
         }
@@ -77,6 +152,8 @@ impl Field {
 }
 
 pub fn maze_solver(maze: &Vec<Vec<DIR>>) -> Option<Vec<String>> {
+    let field = Field::new(maze);
+    dbg!(&field);
     //TODO:
     // create data structure that contains tiles and references on previous directions
     // move to every possible state -> do next turn
@@ -85,7 +162,10 @@ pub fn maze_solver(maze: &Vec<Vec<DIR>>) -> Option<Vec<String>> {
     None
 }
 
-pub fn circular_bit_shift(dir: DIR, shift: i8) -> DIR {
+pub fn shift_dir(dir: DIR, shift: i8) -> DIR {
+    if dir & DIR_MASK != dir {
+        return dir;
+    }
     let shifted = dir << shift;
     if shifted & DIR_MASK == shifted {
         shifted
