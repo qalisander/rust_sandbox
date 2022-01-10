e71e1d6a51daa56259a6c4f32bab8ef7e6836543
Alisander Qoshqosh <qalisander@gmail.com>
2022-01-10 03:33:24 +0300

    transforming_maze_solver: single mutable grid

--- src/transforming_maze_solver/transforming_maze_solver_sol.rs
+++ src/transforming_maze_solver/transforming_maze_solver_sol.rs
@@ -1,6 +1,5 @@
 // https://www.codewars.com/kata/5b86a6d7a4dcc13cd900000b/train/rust
 
-use crate::transforming_maze_solver::transforming_maze_solver_sol::Tile::Unvisited;
 use itertools::Itertools;
 use std::fmt::{Debug, Formatter};
 
@@ -18,7 +17,7 @@ const END: DIR = -2;
 enum Tile {
     Visited {
         prev_tile_delta: (i8, i8),
-        prev_grid_index: i8,
+        is_prev_same_interval: bool,
         walls: DIR,
     },
     Unvisited {
@@ -30,48 +29,65 @@ enum Tile {
 
 type Grid = Vec<Vec<Tile>>;
 struct Field {
-    grids: [Grid; 4],
-    index: usize,
+    grid: Grid,
     begin: (i8, i8),
     end: (i8, i8),
+}
+
+impl Field {
+    fn rotate_walls(&mut self) {
+        for tile in self.grid.iter_mut().flatten() {
+            match tile {
+                Tile::Visited { ref mut walls, .. } => {
+                    *walls = shift_dir(*walls, 1);
+                }
+                Tile::Unvisited { ref mut walls, .. } => {
+                    *walls = shift_dir(*walls, 1)
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+pub fn shift_dir(dir: DIR, shift: i8) -> DIR {
+    let shifted = dir << (shift % 4);
+    (shifted & DIR_MASK) | (shifted >> 4)
 }
 
 impl Debug for Field {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         writeln!(f)?;
-        for (index, grid) in self.grids.iter().enumerate() {
-            writeln!(f, "Grid: {}", index)?;
-            let i_max = grid.len();
-            let j_max = grid[0].len();
+        let i_max = self.grid.len();
 
-            let grid = grid
+        let grid = self
+            .grid
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|tile| match *tile {
+                        Tile::Visited {
+                            walls,
+                            prev_tile_delta,
+                            ..
+                        } => format_walls(walls, get_dir_char(prev_tile_delta)),
+                        Tile::Unvisited { walls } => format_walls(walls, '·'),
+                        Tile::Begin => format_walls(0, 'B'),
+                        Tile::End => format_walls(0, 'X'),
+                    })
+                    .collect_vec()
+            })
+            .collect_vec();
+
+        for str_row_index in 0..i_max * 3 {
+            let row_index = str_row_index / 3;
+            let index = str_row_index % 3;
+            let str = grid[row_index]
                 .iter()
-                .map(|row| {
-                    row.iter()
-                        .map(|tile| match *tile {
-                            Tile::Visited {
-                                walls,
-                                prev_tile_delta,
-                                ..
-                            } => format_walls(walls, get_dir_char(prev_tile_delta)),
-                            Tile::Unvisited { walls } => format_walls(walls, '·'),
-                            Tile::Begin => format_walls(0, 'B'),
-                            Tile::End => format_walls(0, 'X'),
-                        })
-                        .collect_vec()
-                })
-                .collect_vec();
-
-            for str_row_index in 0..i_max * 3 {
-                let row_index = str_row_index / 3;
-                let index = str_row_index % 3;
-                let str = grid[row_index]
-                    .iter()
-                    .flat_map(|arr| arr[3 * index..3 * (index + 1)].iter())
-                    .intersperse(&' ')
-                    .collect::<String>();
-                writeln!(f, "{}", str)?;
-            }
+                .flat_map(|arr| arr[3 * index..3 * (index + 1)].iter())
+                .intersperse(&' ')
+                .collect::<String>();
+            writeln!(f, "{}", str)?;
         }
         return writeln!(f);
 
@@ -101,6 +117,7 @@ fn get_dir_char(delta: (i8, i8)) -> char {
     }
 }
 
+
 impl Field {
     fn new(maze: &Vec<Vec<DIR>>) -> Self {
         let mut begin = None;
@@ -118,50 +135,40 @@ impl Field {
             dir => panic!("Invalid cell! value: {}; index: {:?}", dir, (i, j)),
         };
 
-        let grid = (0..4)
-            .map(|shift| {
-                maze.iter()
+        let grid = maze
+            .iter()
+            .enumerate()
+            .map(|(i, row)| {
+                row.iter()
                     .enumerate()
-                    .map(|(i, row)| {
-                        row.iter()
-                            .enumerate()
-                            .map(|(j, &dir)| {
-                                let dir = if matches!(dir, END | BEGIN) {
-                                    dir
-                                } else {
-                                    shift_dir(dir, shift)
-                                };
-                                process_dir(dir, (i, j))
-                            })
-                            .collect_vec()
-                    })
+                    .map(|(j, &dir)| process_dir(dir, (i, j)))
                     .collect_vec()
             })
-            .collect_vec()
-            .try_into()
-            .unwrap();
+            .collect_vec();
 
         Field {
-            grids: grid,
-            index: 0,
+            grid,
             begin: begin.expect("Begin cell not found!"),
             end: end.expect("End cell not found!"),
         }
     }
 }
 
+struct State {
+    current: (i8, i8),
+    prev: (i8, i8),
+}
+
 pub fn maze_solver(maze: &Vec<Vec<DIR>>) -> Option<Vec<String>> {
     let field = Field::new(maze);
     dbg!(&field);
+
+
     //TODO:
-    // create data structure that contains tiles and references on previous directions
-    // move to every possible state -> do next turn
-    // move back from end tile to beginning
+    // [x] create data structure that contains tiles and references on previous directions
+    // [ ] move to every possible state -> do next turn
+    // [ ] move back from end tile to beginning
 
     None
 }
 
-pub fn shift_dir(dir: DIR, shift: i8) -> DIR {
-    let shifted = dir << (shift % 4);
-    (shifted & DIR_MASK) | (shifted >> 4)
-}
