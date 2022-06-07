3afcd695cbc4e0063f203762d5b884409cbcabd9
Alisander Qoshqosh <qalisander@gmail.com>
2022-06-07 21:15:19 +0400

    fluid_volume_of_a_heightmap.rs: solution

--- src/fluid_volume_of_a_heightmap.rs
+++ src/fluid_volume_of_a_heightmap.rs
@@ -24,84 +24,76 @@ impl Cell {
 
 // 80 <= heightmap <= 100 and -50 <= depth <= 150
 fn volume(heightmap: &Vec<Vec<i32>>) -> i32 {
+    let i_max = heightmap.len();
+    let j_max = heightmap[0].len();
     let mut total_volume = 0;
     let mut heightmap = heightmap.clone();
     let cells = heightmap.iter().enumerate().flat_map(|(i, raw)| {
         raw.iter()
             .enumerate()
-            .map(move |(j, heigh)| (*heigh, (i, j)))
+            .map(move |(j, heigh)| Cell::new(*heigh, (i, j)))
     });
-    let sorted_cells = cells
-        .into_group_map()
-        .into_iter()
-        .sorted_by_key(|(k, vec)| *k);
-    let i_max = heightmap.len();
-    let j_max = heightmap[0].len();
-    let mut increased_height:Vec<(usize, usize)> = vec![]; //increased_height_cells
-    for (height, cells) in sorted_cells {
-        let mut visited: HashSet<(usize, usize)> = HashSet::new();
-        let mut floor_volume = 0;
-        let cells = cells
-            .into_iter()
-            .chain(increased_height.drain(..))
-            .collect_vec();
-        for (i0, j0) in cells {
-            if visited.contains(&(i0, j0)) {
+    let mut cells_heap = BinaryHeap::from_iter(cells);
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut prev_height = cells_heap.peek().unwrap().height();
+    while let Some(cell) = cells_heap.pop() {
+        if prev_height != cell.height() {
+            visited.clear()
+        }
+        if visited.contains(&cell.index) {
+            continue;
+        }
+
+        let mut has_increase_height = true;
+        let mut increase_height_candidates = vec![];
+        let mut deque = VecDeque::from([cell.index]);
+        while let Some((i, j)) = deque.pop_front() {
+            if visited.contains(&(i, j)) {
                 continue;
             }
+            increase_height_candidates.push((i, j));
+            visited.insert((i, j));
 
-            let mut deque = VecDeque::from([(i0, j0)]);
-            let mut increase_height_candidates = vec![];
-            let mut has_increase_height = true;
-            while let Some((i, j)) = deque.pop_front() {
-                if visited.contains(&(i, j)) {
-                    continue;
-                }
-                increase_height_candidates.push((i, j));
-                visited.insert((i, j));
+            let in_bounds =
+                |&(i, j): &(i32, i32)| 0 <= i && i < i_max as i32 && 0 <= j && j < j_max as i32;
+            let has_same_height = |&(ni, nj): &(usize, usize)| heightmap[ni][nj] == heightmap[i][j];
+            let has_lower_height = |&(ni, nj): &(usize, usize)| heightmap[ni][nj] < heightmap[i][j];
+            let to_usize = |(i, j): (i32, i32)| (i as usize, j as usize);
 
-                let in_bounds =
-                    |&(i, j): &(i32, i32)| 0 <= i && i < i_max as i32 && 0 <= j && j < j_max as i32;
-                let has_same_height =
-                    |&(ni, nj): &(usize, usize)| heightmap[ni][nj] == heightmap[i][j];
-                let has_lower_height =
-                    |&(ni, nj): &(usize, usize)| heightmap[ni][nj] < heightmap[i][j];
-                let to_usize = |(i, j): (i32, i32)| (i as usize, j as usize);
-
-                let dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-                let next_points = dirs
-                    .into_iter()
-                    .map(|(di, dj)| (i as i32 + di, j as i32 + dj))
-                    .collect_vec();
-
-                if has_increase_height {
-                    has_increase_height = next_points.iter().all(in_bounds)
-                        && !next_points
-                            .iter()
-                            .cloned()
-                            .filter(in_bounds)
-                            .map(to_usize)
-                            .any(|p| has_lower_height(&p));
-                }
-
-                let to_visit = next_points
-                    .into_iter()
-                    .filter(in_bounds)
-                    .map(to_usize)
-                    .filter(has_same_height);
-                deque.extend(to_visit);
-            }
+            let dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+            let next_points = dirs
+                .into_iter()
+                .map(|(di, dj)| (i as i32 + di, j as i32 + dj))
+                .collect_vec();
 
             if has_increase_height {
-                for &(i, j) in &increase_height_candidates {
-                    heightmap[i][j] += 1;
-                    floor_volume += 1;
-                }
-                increased_height.extend(increase_height_candidates.into_iter());
+                has_increase_height = next_points.iter().all(in_bounds)
+                    && !next_points
+                        .iter()
+                        .cloned()
+                        .filter(in_bounds)
+                        .map(to_usize)
+                        .any(|p| has_lower_height(&p));
+            }
+
+            let to_visit = next_points
+                .into_iter()
+                .filter(in_bounds)
+                .map(to_usize)
+                .filter(has_same_height);
+            deque.extend(to_visit);
+        }
+
+        prev_height = cell.height();
+        if has_increase_height {
+            for (i, j) in increase_height_candidates {
+                heightmap[i][j] += 1;
+                total_volume += 1;
+                cells_heap.push(Cell::new(cell.height() + 1, (i, j)))
             }
         }
-        total_volume += floor_volume;
     }
+
     total_volume
 }
 
@@ -126,10 +118,10 @@ mod tests {
     #[test]
     fn small_maps_test() {
         let tests = [
-//            (vec![vec![0]], 0),
-//            (vec![vec![22]], 0),
-//            (vec![vec![2, 1, 2], vec![1, 0, 1], vec![2, 1, 2]], 1),
-//            (vec![vec![1, 1, 1], vec![1, 8, 1], vec![1, 1, 1]], 0),
+            (vec![vec![0]], 0),
+            (vec![vec![22]], 0),
+            (vec![vec![2, 1, 2], vec![1, 0, 1], vec![2, 1, 2]], 1),
+            (vec![vec![1, 1, 1], vec![1, 8, 1], vec![1, 1, 1]], 0),
             (
                 vec![
                     vec![9, 9, 9, 9],
@@ -139,83 +131,83 @@ mod tests {
                 ],
                 36,
             ),
-//            (
-//                vec![
-//                    vec![9, 9, 9, 9, 9],
-//                    vec![9, 0, 1, 2, 9],
-//                    vec![9, 7, 8, 3, 9],
-//                    vec![9, 6, 5, 4, 9],
-//                    vec![9, 9, 9, 9, 9],
-//                ],
-//                45,
-//            ),
-//            (
-//                vec![
-//                    vec![8, 8, 8, 8, 6, 6, 6, 6],
-//                    vec![8, 0, 0, 8, 6, 0, 0, 6],
-//                    vec![8, 0, 0, 8, 6, 0, 0, 6],
-//                    vec![8, 8, 8, 8, 6, 6, 6, 0],
-//                ],
-//                56,
-//            ),
-//            (
-//                vec![
-//                    vec![0, 10, 0, 20, 0],
-//                    vec![20, 0, 30, 0, 40],
-//                    vec![0, 40, 0, 50, 0],
-//                    vec![50, 0, 60, 0, 70],
-//                    vec![0, 60, 0, 70, 0],
-//                ],
-//                150,
-//            ),
-//            (
-//                vec![
-//                    vec![3, 3, 3, 3, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 3, 3, 0, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 0, 3, 3, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 3, 3, 0, 3],
-//                ],
-//                0,
-//            ),
-//            (
-//                vec![
-//                    vec![3, 3, 3, 3, 3],
-//                    vec![3, 2, 2, 2, 3],
-//                    vec![3, 3, 3, 2, 3],
-//                    vec![3, 1, 1, 1, 3],
-//                    vec![3, 1, 3, 3, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 3, 3, 0, 3],
-//                ],
-//                0,
-//            ),
-//            (
-//                vec![
-//                    vec![3, 3, 3, 3, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 3, 3, 0, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 0, 3, 3, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 3, 3, 1, 3],
-//                ],
-//                11,
-//            ),
-//            (
-//                vec![
-//                    vec![3, 3, 3, 1, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 0, 3, 3, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 3, 3, 0, 3],
-//                    vec![3, 0, 0, 0, 3],
-//                    vec![3, 3, 3, 3, 3],
-//                ],
-//                11,
-//            ),
+            (
+                vec![
+                    vec![9, 9, 9, 9, 9],
+                    vec![9, 0, 1, 2, 9],
+                    vec![9, 7, 8, 3, 9],
+                    vec![9, 6, 5, 4, 9],
+                    vec![9, 9, 9, 9, 9],
+                ],
+                45,
+            ),
+            (
+                vec![
+                    vec![8, 8, 8, 8, 6, 6, 6, 6],
+                    vec![8, 0, 0, 8, 6, 0, 0, 6],
+                    vec![8, 0, 0, 8, 6, 0, 0, 6],
+                    vec![8, 8, 8, 8, 6, 6, 6, 0],
+                ],
+                56,
+            ),
+            (
+                vec![
+                    vec![0, 10, 0, 20, 0],
+                    vec![20, 0, 30, 0, 40],
+                    vec![0, 40, 0, 50, 0],
+                    vec![50, 0, 60, 0, 70],
+                    vec![0, 60, 0, 70, 0],
+                ],
+                150,
+            ),
+            (
+                vec![
+                    vec![3, 3, 3, 3, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 3, 3, 0, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 0, 3, 3, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 3, 3, 0, 3],
+                ],
+                0,
+            ),
+            (
+                vec![
+                    vec![3, 3, 3, 3, 3],
+                    vec![3, 2, 2, 2, 3],
+                    vec![3, 3, 3, 2, 3],
+                    vec![3, 1, 1, 1, 3],
+                    vec![3, 1, 3, 3, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 3, 3, 0, 3],
+                ],
+                0,
+            ),
+            (
+                vec![
+                    vec![3, 3, 3, 3, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 3, 3, 0, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 0, 3, 3, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 3, 3, 1, 3],
+                ],
+                11,
+            ),
+            (
+                vec![
+                    vec![3, 3, 3, 1, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 0, 3, 3, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 3, 3, 0, 3],
+                    vec![3, 0, 0, 0, 3],
+                    vec![3, 3, 3, 3, 3],
+                ],
+                11,
+            ),
         ];
 
         for (map, expected) in tests.iter() {
